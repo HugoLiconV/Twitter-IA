de9b210ca83b967e8a98c3ddc17b33c5b72c24a9
Hugo <hugo_val28@hotmail.com>
2018-10-31 00:07:32 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tweeter API
+## Installation
+
---
+++ main.py
@@ -0,0 +1,72 @@
+import sys
+import tweepy
+import os
+from dotenv import load_dotenv
+
+# Load Environment variables for tokens
+load_dotenv()
+
+SECRET_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+
+auth = tweepy.OAuthHandler(SECRET_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth, wait_on_rate_limit=True,
+                 wait_on_rate_limit_notify=True)
+
+if not api:
+    print("Can't Authenticate")
+    sys.exit(-1)
+
+# Continue with rest of code
+searchQuery = '#4MinutosEnZDU'  # this is what we're searching for
+maxTweets = 10000000  # Some arbitrary large number
+tweetsPerQry = 100  # this is the max the API permits
+fName = 'tweets.txt'  # We'll store the tweets in a text file.
+
+# If results from a specific ID onwards are reqd, set since_id to that ID.
+# else default to no lower limit, go as far back as API allows
+sinceId = None
+
+# If results only below a specific ID are, set max_id to that ID.
+# else default to no upper limit, start from the most recent tweet matching the search query.
+max_id = -1
+
+tweetCount = 0
+print("Downloading max {0} tweets".format(maxTweets))
+with open(fName, 'wb') as f:
+    while tweetCount < maxTweets:
+        try:
+            if max_id <= 0:
+                if not sinceId:
+                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry)
+                else:
+                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                            since_id=sinceId)
+            else:
+                if not sinceId:
+                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                            max_id=str(max_id - 1))
+                else:
+                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                            max_id=str(max_id - 1),
+
+                                            since_id=sinceId)
+            if not new_tweets:
+                print("No more tweets found")
+                break
+            for tweet in new_tweets:
+                f.write(tweet.text.encode("utf-8"))
+                print(tweet.text)
+            tweetCount += len(new_tweets)
+            print("Downloaded {0} tweets".format(tweetCount))
+            max_id = new_tweets[-1].id
+        except tweepy.TweepError as e:
+            # Just exit if any error
+            print("some error : " + str(e))
+            break
+
+print("Downloaded {0} tweets, Saved to {1}".format(tweetCount, fName))
